77bd7853a4c8bb7dd5f2ffa987aacae31178216c
Your Name <yudhaxsinatra@gmail.com>
2022-05-05 20:51:59 +0700

    Update : Add Modal Register, Login Page

---
+++ resources/views/main/singlePage.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('container')
+    <div class="container px-5">
+        <div class="details">
+            <h1 class="text-2xl">Nama Produk</h1>
+            <img class="border-rose-500 rounded-lg border-rose-500 border-4 w-72"
+                src="https://id-test-11.slatic.net/p/f01403bdf57a55e132ee1c59765ebfc4.jpg_720x720q80.jpg_.webp"
+                alt="Image Product">
+        </div>
+    </div>
+@endsection
